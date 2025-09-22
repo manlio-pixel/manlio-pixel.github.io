11c40da2ee81d71d7d688421f640732c714fe330
manlio-pixel <manliowhatsapp@gmail.com>
2025-09-21 19:45:06 -0600

    commit changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 // 🔧 Control de mantenimiento
-const mantenimiento = false; // cámbialo a true para activar
+const mantenimiento = true; // cámbialo a true para activar
 
 if (mantenimiento) {
   window.location.href = "mantenimiento.html";
 }
+
